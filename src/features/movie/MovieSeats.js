b5a60a411b3c3b42a891b7dc96be5a6e599e4959
vienpham2019 <vienpham2019@gmail.com>
2024-04-09 12:37:32 -0500

    ad movie seat

---
+++ src/features/movie/MovieSeats.js
@@ -0,0 +1,99 @@
+import { FaWheelchair } from "react-icons/fa6";
+
+const MovieSeats = ({ sitLayOut = [], selectedSeats = [] }) => {
+  const disPlaySitLayOut = () => {
+    return Object.keys(sitLayOut).map((key, index) => {
+      if (key.startsWith("Hall")) {
+        return (
+          <div key={"sit hall" + index} className="flex-1 p-[0.5rem]"></div>
+        );
+      }
+      let sitCount = 0;
+      return (
+        <div
+          key={"sit row" + key}
+          className="flex justify-center flex-1 h-[2rem] gap-2"
+        >
+          <div
+            key={"row " + key}
+            className="w-[2rem] aspect-square flex justify-center items-center font-thin text-cyan-200"
+          >
+            {key !== "SitCount" && key}
+          </div>
+          {sitLayOut[key].map((sit) => {
+            let sitAmount = parseInt(sit, 10); // Parse the string as an integer with base 10
+            let sitType = sit.charAt(sit.length - 1); // Get the last character of the string
+            if (sitType === "H") {
+              return Array.from({ length: sitAmount }).map((_, i) => (
+                <div
+                  key={"hall " + key + i}
+                  className="w-[2rem] aspect-square"
+                ></div>
+              ));
+            }
+            return Array.from({ length: sitAmount }).map((_, i) =>
+              SitComponent({ sitNumber: key + ++sitCount, sitType })
+            );
+          })}
+        </div>
+      );
+    });
+  };
+
+  const SitComponent = ({ sitNumber, sitType }) => {
+    let sitClass =
+      "w-[2rem] aspect-square rounded-md flex justify-center items-center cursor-pointer";
+    if (selectedSeats.includes(sitNumber)) {
+      sitClass += " bg-black ";
+    } else {
+      sitClass += " hover:bg-cyan-600 border border-gray-400";
+    }
+
+    return (
+      <div
+        key={"sit " + sitNumber}
+        className={sitClass}
+        onClick={() => console.log(sitNumber)}
+      >
+        {sitType === "D" && <FaWheelchair />}
+      </div>
+    );
+  };
+  return (
+    <div className=" overflow-x-scroll tablet:max-w-[47rem] mobile:w-[19rem] px-5 bg-[#172532] rounded-md">
+      <div className="flex flex-col gap-1 py-2 min-w-[40rem]">
+        <div className="flex flex-col items-center m-2 pl-[2rem]">
+          <div className="w-[30rem] h-[5rem] screen-container ">
+            <div className="screen w-full bg-gray-400"></div>
+          </div>
+          <div className="w-[30rem] h-[5rem] screen-container ">
+            <div className="screen-shadow w-full bg-gradient-to-b from-gray-700"></div>
+          </div>
+        </div>
+        {disPlaySitLayOut()}
+        <div className="h-[2rem] mt-2 flex flex-1 justify-around font-thin">
+          <div className="flex gap-2 items-center text-gray-300">
+            <div className="w-[2rem] aspect-square border border-gray-400 rounded-md flex justify-center items-center cursor-pointer hover:bg-cyan-500"></div>
+            <span>Available</span>
+          </div>
+          <div className="flex gap-2 items-center text-gray-300">
+            <div className="w-[2rem] aspect-square border border-gray-400 rounded-md flex justify-center items-center cursor-pointer hover:bg-cyan-500">
+              <FaWheelchair />
+            </div>
+            <span>Handicap</span>
+          </div>
+          <div className="flex gap-2 items-center text-gray-300">
+            <div className="w-[2rem] bg-cyan-600 aspect-square border border-gray-400 rounded-md flex justify-center items-center cursor-pointer hover:bg-cyan-500"></div>
+            <span>Selected</span>
+          </div>
+          <div className="flex gap-2 items-center text-gray-300">
+            <div className="w-[2rem] bg-black aspect-square rounded-md flex justify-center items-center cursor-pointer hover:bg-cyan-500"></div>
+            <span>Taken</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieSeats;
--- src/features/movie/MovieTicket.js
+++ src/features/movie/MovieTicket.js
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import { useGetMovieByIdQuery } from "./movieApiSlice";
 import DateSlider from "../../components/slider/DateSlider";
 import Selection from "../../components/form/Selection";
-import { FaWheelchair } from "react-icons/fa6";
+import MovieSeats from "./MovieSeats";
 
 const MovieTicket = () => {
   const { movieId } = useParams();
@@ -25,56 +25,28 @@ const MovieTicket = () => {
     F: ["5N", "1H", "5N", "1H", "5N"],
     G: ["5N", "1H", "5N", "1H", "5N"],
     H: ["5N", "1H", "5N", "1H", "5N"],
-    SitCount: ["5N", "1H", "5N", "1H", "5N"],
   };
 
-  const disPlaySitLayOut = () => {
-    return Object.keys(sitLayOut).map((key, index) => {
-      if (key.startsWith("Hall")) {
-        return (
-          <div key={"sit hall" + index} className="flex-1 p-[0.5rem]"></div>
-        );
-      }
-      let sitCount = 0;
-      return (
-        <div
-          key={"sit row" + key}
-          className="flex justify-center flex-1 h-[2rem] gap-2"
-        >
-          <div
-            key={"row " + key}
-            className="w-[2rem] aspect-square flex justify-center items-center text-gray-400"
-          >
-            {key !== "SitCount" && key}
-          </div>
-          {sitLayOut[key].map((sit) => {
-            let sitAmount = parseInt(sit, 10); // Parse the string as an integer with base 10
-            let sitType = sit.charAt(sit.length - 1); // Get the last character of the string
-            if (sitType === "H") {
-              return Array.from({ length: sitAmount }).map((_, i) => (
-                <div
-                  key={"hall " + key + i}
-                  className="w-[2rem] aspect-square"
-                ></div>
-              ));
-            }
-            let sitClass =
-              "w-[2rem] aspect-square border border-gray-400 rounded-md flex justify-center items-center cursor-pointer hover:bg-cyan-500";
-            if (key === "SitCount") {
-              sitClass =
-                "w-[2rem] aspect-square flex justify-center items-center text-gray-400";
-            }
-            return Array.from({ length: sitAmount }).map((_, i) => (
-              <div key={"sit " + key + i} className={sitClass}>
-                {key === "SitCount" && ++sitCount}
-                {sitType === "D" && <FaWheelchair />}
-              </div>
-            ));
-          })}
-        </div>
-      );
-    });
-  };
+  const selectedSeats = [
+    "A1",
+    "A2",
+    "A3",
+    "A5",
+    "A6",
+    "A7",
+    "C1",
+    "C2",
+    "C6",
+    "C7",
+    "C8",
+    "C9",
+    "D4",
+    "D5",
+    "D6",
+    "D7",
+    "D8",
+    "D9",
+  ];
 
   if (isLoading || !movie) return <div>Loading</div>;
 
@@ -149,14 +121,7 @@ const MovieTicket = () => {
           </div>
 
           {/* Sit */}
-          <div className="flex flex-col gap-1 w-full py-2">
-            <div className="flex justify-center m-2 pl-[2rem]">
-              <div className="w-[45rem] h-[2rem] border border-cyan-200 border-b-0 shadow-lg bg-gradient-to-b from-cyan-800 rounded-t-full text-[0.7rem] flex justify-center items-center text-gray-400">
-                Screen
-              </div>
-            </div>
-            {disPlaySitLayOut()}
-          </div>
+          <MovieSeats sitLayOut={sitLayOut} selectedSeats={selectedSeats} />
           {/* Sit */}
         </div>
       </div>
